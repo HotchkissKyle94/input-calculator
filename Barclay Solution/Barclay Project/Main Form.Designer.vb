68ed5533c0e0cb526076b2c58bc36e1821852c5b
Kyle Hotchkiss <44444223+HotchkissKyle94@users.noreply.github.com>
2018-10-26 14:51:56 -0400

    Add files via upload

---
+++ Barclay Solution/Barclay Project/Main Form.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstPrices = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.lblPrice = New System.Windows.Forms.TextBox()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(15, 26)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 17)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "&Prices:"
+        '
+        'lstPrices
+        '
+        Me.lstPrices.FormattingEnabled = True
+        Me.lstPrices.ItemHeight = 17
+        Me.lstPrices.Location = New System.Drawing.Point(19, 80)
+        Me.lstPrices.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.lstPrices.Name = "lstPrices"
+        Me.lstPrices.Size = New System.Drawing.Size(109, 140)
+        Me.lstPrices.TabIndex = 0
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(222, 80)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(65, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Total due:"
+        '
+        'lblPrice
+        '
+        Me.lblPrice.Enabled = False
+        Me.lblPrice.Location = New System.Drawing.Point(225, 123)
+        Me.lblPrice.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.lblPrice.Name = "lblPrice"
+        Me.lblPrice.Size = New System.Drawing.Size(192, 25)
+        Me.lblPrice.TabIndex = 3
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.AutoSize = True
+        Me.btnCalculate.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.Location = New System.Drawing.Point(225, 187)
+        Me.btnCalculate.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(87, 35)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "&Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.AutoSize = True
+        Me.btnExit.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(330, 187)
+        Me.btnExit.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(87, 35)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(464, 251)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblPrice)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.lstPrices)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Barclay Candies"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstPrices As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents lblPrice As TextBox
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Barclay Solution/Barclay Project/Main Form.vb
@@ -0,0 +1,47 @@
+﻿'Name:              Barclay Candies calculator
+'Purpose:           Calculate and display candies purchased 
+'
+'Programmer:        Kyle Hotchkiss on 07/15/2018
+
+
+Option Explicit On
+Option Infer On
+Option Strict On
+
+Public Class frmMain
+
+    Const strPrice As String = "Enter a price: "
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Static dmlTaxRate As Double = 0.05
+        Dim dblTotal As Double
+        Dim strCost As String
+        Dim dblPrice As Double
+        Dim blnIsParse As Boolean
+        Dim dblSalesTax As Double
+
+        strCost = InputBox(strPrice, "Barclay Candies")
+
+        Do While strCost <> String.Empty
+            blnIsParse = Double.TryParse(strCost, dblPrice)
+            If blnIsParse Then
+                lstPrices.Items.Add(Format(dblPrice, "c2"))
+                dblSalesTax = (dblPrice * dmlTaxRate) + dblPrice
+                dblTotal += dblSalesTax
+                lblPrice.Text = dblTotal.ToString("c2")
+            Else
+                MessageBox.Show("Enter a valid number",
+                                "Barclay Candies",
+                                 MessageBoxButtons.OKCancel,
+                                 MessageBoxIcon.Information)
+            End If
+            strCost = InputBox(strPrice, "Barclay Candies")
+        Loop
+
+
+    End Sub
+End Class
